b3d2e027b4da47bb20b87a2818923186457f8fa0
TranAn04 <145312029+TranAn04@users.noreply.github.com>
2025-06-05 23:08:42 +0700

    Update app.py

--- Backend API/app.py
+++ Backend API/app.py
@@ -4,13 +4,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  
 
-@app.route('/api/about')
+@app.route('/about')
 def about():
     return jsonify({
         "name": "Trần Hoài An",
         "job": "Lập trình viên",
         "bio": "Tôi thích học Flask và phát triển phần mềm."
     })
-
+import os
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
